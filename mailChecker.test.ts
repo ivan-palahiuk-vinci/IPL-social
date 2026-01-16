3f877612adfdd8772847cc255b12c95e5d441f4c
ivan-palahiuk-vinci <ivan.palahiuk@student.vinci.be>
2026-01-16 16:35:46 +0100

    Refactoring test order

--- mailChecker.test.ts
+++ mailChecker.test.ts
@@ -5,8 +5,9 @@ describe("Testing isValidEmail function", () => {
     test('Error : No @', () => {
         expect(isValidEmail('ivanvinci.be')).toBe(false);
     });
-    test('Error: No dot in domaine', () => {
-        expect(isValidEmail('ivan@vincibe')).toBe(false);
+    test('Error : Nothing before or after the @', () => {
+        expect(isValidEmail('@vinci.be')).toBe(false);
+        expect(isValidEmail('ivan@')).toBe(false); 
     });
     test('Error : Dot as last character', () => {
         expect(isValidEmail('ivan@vinci.')).toBe(false);
@@ -14,9 +15,8 @@ describe("Testing isValidEmail function", () => {
     test('Error : space error', () => {
         expect(isValidEmail('iv an@vinci.be')).toBe(false);
     });
-    test('Error : Nothing before or after the @', () => {
-        expect(isValidEmail('@vinci.be')).toBe(false);
-        expect(isValidEmail('ivan@')).toBe(false); 
+    test('Error: No dot in domaine', () => {
+        expect(isValidEmail('ivan@vincibe')).toBe(false);
     });
     test('Valid Email test@vinci.be', () => {
         expect(isValidEmail('ivan@vinci.be')).toBe(true);
