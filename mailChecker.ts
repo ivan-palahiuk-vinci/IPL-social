033656f385f9a36e961aced5c3827c359ee09cb0
ivan-palahiuk-vinci <ivan.palahiuk@student.vinci.be>
2026-01-16 16:36:41 +0100

    Test 2 passed

--- mailChecker.ts
+++ mailChecker.ts
@@ -1,5 +1,12 @@
 function isValidEmail(email: string): boolean {
     if(!email.includes('@')) return false;
+
+    const parts = email.split('@');
+    if (parts.length !== 2) return false;
+
+    const [local, domain] = parts;
+    if (local.length === 0 || domain.length === 0) return false;
+
     
     return false;
 }
